90844b7c9b2a40f0367e615ee61aeeb745c7661c
Vixcity <2091283625@qq.com>
2024-09-21 15:25:03 +0800

    feat: 最小堆实现

--- packages/scheduler/__tests__/SchedulerMinHeap.spec.ts
+++ packages/scheduler/__tests__/SchedulerMinHeap.spec.ts
@@ -1,4 +1,4 @@
-import { describe, descript, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { peek, push, pop, Heap, Node } from "../src/SchedulerMinHeap";
 
 let idCounter = 0;
@@ -18,17 +18,17 @@ describe("text min heap", () => {
 
   it("heap length === 1", () => {
     const tasks: Heap<Node> = [createNode(1)];
-    expect(peek(tasks)).toEqual(createNode(1));
+    expect(peek(tasks)?.sortIndex).toEqual(1);
   });
 
   it("heap length > 1", () => {
     const tasks: Heap<Node> = [createNode(1)];
     push(tasks, createNode(2));
     push(tasks, createNode(3));
-    expect(peek(tasks)).toEqual(createNode(1));
+    expect(peek(tasks)?.sortIndex).toEqual(1);
     push(tasks, createNode(0));
-    expect(peek(tasks)).toEqual(createNode(0));
+    expect(peek(tasks)?.sortIndex).toEqual(0);
     pop(tasks);
-    expect(peek(tasks)).toEqual(createNode(1));
+    expect(peek(tasks)?.sortIndex).toEqual(1);
   });
 });
--- packages/scheduler/src/SchedulerMinHeap.ts
+++ packages/scheduler/src/SchedulerMinHeap.ts
@@ -35,8 +35,56 @@ function siftUP<T extends Node>(heap: Heap<T>, node: T, i: number): void {
   }
 }
 
+function siftDown<T extends Node>(heap: Heap<T>, node: T, i: number): void {
+  let index = i;
+  const length = heap.length;
+  const halfLength = length >>> 1;
+  while (index < halfLength) {
+    const leftIndex = (index + 1) * 2 - 1;
+    const left = heap[leftIndex]; // 左子节点
+    const rightIndex = leftIndex + 1;
+    const right = heap[rightIndex]; // 右子节点, 不一定存在，需要判断是否存在
+    if (compare(left, node) < 0) {
+      // left < node
+      if (rightIndex < length && compare(right, left) < 0) {
+        // right 存在，且 right < left
+        heap[index] = right;
+        heap[rightIndex] = node;
+        index = rightIndex;
+      } else {
+        // right 不存在或 right >= left
+        heap[index] = left;
+        heap[leftIndex] = node;
+        index = leftIndex;
+      }
+    } else if (rightIndex < length && compare(right, node) < 0) {
+      // right 存在，且 right < node
+      heap[index] = right;
+      heap[rightIndex] = node;
+      index = rightIndex;
+    } else {
+      // 根节点最小
+      return;
+    }
+  }
+}
+
 // 删除堆顶元素
-export function pop() {}
+export function pop<T extends Node>(heap: Heap<T>): T | null {
+  if (heap.length === 0) return null;
+
+  // 1. 取出堆顶元素
+  const first = heap[0];
+  // 2. 把最后一个元素放到堆顶
+  const last = heap.pop()!;
+  if (first !== last) {
+    // heap 中有2个或者多个元素
+    heap[0] = last;
+    siftDown(heap, last, 0);
+  }
+
+  return first;
+}
 
 function compare(a: Node, b: Node) {
   const diff = a.sortIndex - b.sortIndex;
